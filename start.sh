02b8b8bc17ddb30d863b2ac5599bf6037eba74e1
changehow <changehow1122@gmail.com>
2023-11-04 13:38:27 +0800

    fix: 🐛 just install deps by volta

--- start.sh
+++ start.sh
@@ -10,13 +10,7 @@ then
     exit 1
 fi
 
-# 检查 pnpm 是否已经安装
-if ! volta list pnpm &> /dev/null
-then
-    echo "缺少 pnpm，正在安装"
-    # 如果 pnpm 没有安装，那么安装它
-    volta install pnpm
-fi
+volta install pnpm
 
 # 使用 pnpm 安装依赖
 pnpm install
@@ -24,13 +18,8 @@ pnpm install
 # 使用 pnpm 编译项目
 pnpm run build
 
-# 检查 pm2 是否已经安装
-if ! volta list pm2 &> /dev/null
-then
-    echo "缺少 pm2，正在安装"
-    # 如果 pm2 没有安装，那么安装它
-    volta install pm2
-fi
+
+volta install pm2
 
 # 清理所有 pm2 进程
 pm2 delete all
