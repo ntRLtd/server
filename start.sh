93477f21b1d042d39c0a46ba58b3fe13dc2142f8
changehow <changehow1122@gmail.com>
2023-11-04 13:33:13 +0800

    fix: 🐛 update script

--- start.sh
+++ start.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# 检查 volta 是否已经安装
+if ! command -v volta &> /dev/null
+then
+    # 如果 volta 没有安装，那么安装它
+    curl https://get.volta.sh | bash
+    # 提示用户重新运行脚本
+    echo "Volta 安装完成，请重新运行 start.sh 启动服务"
+    exit 1
+fi
+
 # 检查 pnpm 是否已经安装
-if ! command -v pnpm &> /dev/null
+if ! volta list pnpm &> /dev/null
 then
     # 如果 pnpm 没有安装，那么安装它
-    npm install -g pnpm
+    volta install pnpm
 fi
 
 # 使用 pnpm 安装依赖
@@ -14,10 +24,10 @@ pnpm install
 pnpm run build
 
 # 检查 pm2 是否已经安装
-if ! command -v pm2 &> /dev/null
+if ! volta list pm2 &> /dev/null
 then
     # 如果 pm2 没有安装，那么安装它
-    pnpm install -g pm2
+    volta install pm2
 fi
 
 # 清理所有 pm2 进程
@@ -25,4 +35,3 @@ pm2 delete all
 
 # 启动 pm2
 pm2 start ecosystem.config.js
-
